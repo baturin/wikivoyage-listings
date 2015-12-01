9e42f5dd30fedfa1c26dff5d6f4a7c3da6a6a82c
Alexey Baturin <alexey.baturin.nsk@gmail.com>
2015-12-01 23:40:42 +0600

    Move html template out of code

--- src/main/java/org/wikivoyage/listings/output/ValidationReport.java
+++ src/main/java/org/wikivoyage/listings/output/ValidationReport.java
@@ -1,5 +1,6 @@
 package org.wikivoyage.listings.output;
 
+import org.apache.commons.io.IOUtils;
 import org.wikivoyage.listings.entity.WikivoyagePOI;
 import org.wikivoyage.listings.validators.*;
 
@@ -19,43 +20,30 @@ public class ValidationReport implements OutputFormat {
         };
 
         try {
+            StringBuilder rows = new StringBuilder();
+            for (WikivoyagePOI poi: pois) {
+                for (Validator validator: validators) {
+                    String errorMessage = validator.validate(poi);
+                    if (errorMessage != null) {
+                        String row = "<tr><td><a href='http://" + poi.getLanguage() + ".wikivoyage.org/wiki/" +
+                                URLEncoder.encode(poi.getArticle().replace(" ", "_"), "UTF-8") + "'>"
+                                + poi.getArticle() + "</a></td><td>" + poi.getTitle() + "</td>" +
+                                "<td>" + errorMessage + "</td></tr>\n";
+                        rows.append(row);
+                    }
+                }
+            }
+
+            String template = IOUtils.toString(
+                this.getClass().getResourceAsStream("/validation-report-template.htm"), "UTF-8"
+            );
+
             BufferedWriter writer = null;
 
             try {
                 FileWriter fwriter = new FileWriter(outputFilename);
                 writer = new BufferedWriter(fwriter);
-
-
-                writer.write("<!DOCTYPE html>\n");
-                writer.write("<html>\n");
-                writer.write("<head>\n");
-                writer.write("<meta charset=\"utf-8\">");
-                writer.write("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">\n");
-                writer.write("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">\n");
-                writer.write("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\"></script>\n");
-                writer.write("<title>Wikivoyage validation report</title>");
-                writer.write("</head>\n");
-                writer.write("<body>\n");
-                writer.write("<div class=\"container-fluid\">\n");
-                writer.write("<table class=\"table\">\n");
-                writer.write("<tr><th>Article</th><th>Listing</th><th>Issue</th></tr>\n");
-                for (WikivoyagePOI poi: pois) {
-                    for (Validator validator: validators) {
-                        String errorMessage = validator.validate(poi);
-                        if (errorMessage != null) {
-                            writer.write(
-                                "<tr><td><a href='http://" + poi.getLanguage() + ".wikivoyage.org/wiki/" +
-                                URLEncoder.encode(poi.getArticle().replace(" ", "_"), "UTF-8") + "'>"
-                                + poi.getArticle() + "</a></td><td>" + poi.getTitle() + "</td>" +
-                                "<td>" + errorMessage + "</td></tr>"
-                            );
-                        }
-                    }
-                }
-                writer.write("</table>\n");
-                writer.write("</div>\n");
-                writer.write("</body>\n");
-                writer.write("</html>\n");
+                writer.write(template.replace("{rows}", rows.toString()));
             } finally {
                 if (writer != null) {
                     writer.close();
