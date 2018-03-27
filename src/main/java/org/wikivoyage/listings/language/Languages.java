ce1bd5554712ae317f8f4708214e8f31d56d3edb
nsaiisasidhar <sasinimmagadda@gmail.com>
2018-03-27 12:23:02 -0400

    Add Chinese to WikiVoyage languages

--- src/main/java/org/wikivoyage/listings/language/Languages.java
+++ src/main/java/org/wikivoyage/listings/language/Languages.java
@@ -5,6 +5,7 @@ import org.wikivoyage.listings.language.german.German;
 import org.wikivoyage.listings.language.russian.Russian;
 import org.wikivoyage.listings.language.french.French;
 import org.wikivoyage.listings.language.espanol.Espanol;
+import org.wikivoyage.listings.language.chinese.Chinese;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -18,7 +19,8 @@ public class Languages {
         new Russian(),
         new French(),
         new German(),
-		new Espanol()
+	new Espanol(),
+	new Chinese()
     };
     private static Language defaultLanguage = languages[0];
 
