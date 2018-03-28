afc495babfd205b2037a00686bb76cd816b54d0d
nsaiisasidhar <sasinimmagadda@gmail.com>
2018-03-28 15:38:26 -0400

    Add Italian & Chinese

--- tools/statistics.sh
+++ tools/statistics.sh
@@ -3,7 +3,7 @@
 
 # Generate SQLite database and statistics for each language
 mkdir -p listings
-for WVLANGUAGE in fr ru en de es; do
+for WVLANGUAGE in fr ru en de es it zh; do
 
   echo "=== LANGUAGE: $WVLANGUAGE ==="
 
@@ -24,10 +24,14 @@ sqlite3 listings/fr.sqlitedb 'UPDATE wikivoyage_listings SET id = id + 1000000;'
 sqlite3 listings/ru.sqlitedb 'UPDATE wikivoyage_listings SET id = id + 2000000;'
 sqlite3 listings/de.sqlitedb 'UPDATE wikivoyage_listings SET id = id + 3000000;'
 sqlite3 listings/es.sqlitedb 'UPDATE wikivoyage_listings SET id = id + 4000000;'
+sqlite3 listings/it.sqlitedb 'UPDATE wikivoyage_listings SET id = id + 5000000;'
+sqlite3 listings/zh.sqlitedb 'UPDATE wikivoyage_listings SET id = id + 6000000;'
 sqlite3 listings/all.sqlitedb 'attach "listings/fr.sqlitedb" AS toMerge; INSERT INTO wikivoyage_listings SELECT * FROM toMerge.wikivoyage_listings;'
 sqlite3 listings/all.sqlitedb 'attach "listings/ru.sqlitedb" AS toMerge; INSERT INTO wikivoyage_listings SELECT * FROM toMerge.wikivoyage_listings;'
 sqlite3 listings/all.sqlitedb 'attach "listings/de.sqlitedb" AS toMerge; INSERT INTO wikivoyage_listings SELECT * FROM toMerge.wikivoyage_listings;'
 sqlite3 listings/all.sqlitedb 'attach "listings/es.sqlitedb" AS toMerge; INSERT INTO wikivoyage_listings SELECT * FROM toMerge.wikivoyage_listings;'
+sqlite3 listings/all.sqlitedb 'attach "listings/it.sqlitedb" AS toMerge; INSERT INTO wikivoyage_listings SELECT * FROM toMerge.wikivoyage_listings;'
+sqlite3 listings/all.sqlitedb 'attach "listings/zh.sqlitedb" AS toMerge; INSERT INTO wikivoyage_listings SELECT * FROM toMerge.wikivoyage_listings;'
 
 # Statistics
 ALL=`sqlite3 listings/all.sqlitedb 'SELECT COUNT(*) FROM wikivoyage_listings WHERE wikidata != "" AND type == "listing";'`; echo "Total number of listings, all languages: $ALL"
