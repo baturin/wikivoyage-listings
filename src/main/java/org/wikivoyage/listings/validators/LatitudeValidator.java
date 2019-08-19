d8de6149ad41336912ca8322d0c1f271fb3b05f3
nsaiisasidhar <sasinimmagadda@gmail.com>
2019-08-19 12:42:44 -0400

    Update LatitudeValidator.java

--- src/main/java/org/wikivoyage/listings/validators/LatitudeValidator.java
+++ src/main/java/org/wikivoyage/listings/validators/LatitudeValidator.java
@@ -5,9 +5,10 @@ import org.wikivoyage.listings.entity.Listing;
 public class LatitudeValidator extends SimpleValidator {
     @Override
     public void validate(Listing poi) {
-        if (poi.getLatitude() != null && !poi.getLatitude().equals("")) {
+        String latitude = poi.getLatitude();
+        if (latitude!= null && !(latitude.equals(""))) {
             try {
-                Float.parseFloat(poi.getLatitude());
+                Float.parseFloat(latitude);
             } catch (NumberFormatException e) {
                 poi.add(ValidationIssue.INVALID_LATITUDE);
             }
