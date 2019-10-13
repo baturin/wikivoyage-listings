162103e58f58ea7110c3740017596a53f93cd3b5
k3v1n0x90 <k3v1n0x90@gmail.com>
2019-10-13 12:07:52 +0200

    Remove unnecessary StringUtils and instead use already existing apache commons methods

--- src/main/java/org/wikivoyage/listings/language/french/template/PrixTemplateToStringConverter.java
+++ src/main/java/org/wikivoyage/listings/language/french/template/PrixTemplateToStringConverter.java
@@ -1,14 +1,14 @@
 package org.wikivoyage.listings.language.french.template;
 
+import org.apache.commons.lang.StringUtils;
 import org.wikivoyage.listings.input.template.TemplateNode;
 import org.wikivoyage.listings.input.template.TemplateToStringConverter;
-import org.wikivoyage.listings.utils.StringUtils;
 
 public class PrixTemplateToStringConverter implements TemplateToStringConverter {
     @Override
     public String convertToString(TemplateNode template)
     {
-        return StringUtils.joinStrings(template.getPositionalArguments());
+        return StringUtils.join(template.getPositionalArguments().toArray());
     }
 
     public String getTemplateName()
--- src/main/java/org/wikivoyage/listings/language/russian/template/RussianRoadTemplateToStringConverter.java
+++ src/main/java/org/wikivoyage/listings/language/russian/template/RussianRoadTemplateToStringConverter.java
@@ -2,13 +2,14 @@ package org.wikivoyage.listings.language.russian.template;
 
 import org.wikivoyage.listings.input.template.TemplateNode;
 import org.wikivoyage.listings.input.template.TemplateToStringConverter;
-import org.wikivoyage.listings.utils.StringUtils;
+
+import static org.apache.commons.lang.StringUtils.join;
 
 public class RussianRoadTemplateToStringConverter implements TemplateToStringConverter {
     @Override
     public String convertToString(TemplateNode template)
     {
-        return StringUtils.joinStrings(template.getPositionalArguments());
+        return join(template.getPositionalArguments().toArray());
     }
 
     @Override
